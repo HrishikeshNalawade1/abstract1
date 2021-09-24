ecf777d5a0ae626f9f6c94dabf1f76c6bdf5aa72
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-24 11:31:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace abstr
+{
+    abstract class person
+    {
+        public string name;
+        public string address;
+        public int num;
+        public abstract void info();
+    }
+    class teacher : person
+    {
+        public string subject="maths";
+        public teacher(string name, string address, int num)
+        {
+            this.name = name;
+            this.address = address;
+            this.num = num;
+        }
+        public override void info()
+        {
+            Console.WriteLine("name of teacher is:"+name+"\nAddress of teacher :"+address+"\n Phone number:"+num+"\n Subject:"+subject); ;
+    
+        }
+    }
+    class students : person
+    {
+        public int Roll_no = 3;
+        public students(string name, string address, int num)
+        {
+            this.name = name;
+            this.address = address;
+            this.num = num;
+        }
+        public override void info()
+        {
+            Console.WriteLine("\nname of Student is:" + name + "\nAddress of Student :" + address + "\n Phone number:" + num + "\n Roll number:"+Roll_no); 
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            teacher t = new teacher("Rutuja", "kolhapur", 555);
+            t.info();
+            students s = new students("Hrishikesh", "Kolhapur", 566);
+            s.info();
+        }
+    }
+}
